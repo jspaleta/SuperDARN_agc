e6fd61c61f8581d49a520f37a578954c478041d4
Jef Spaleta <jspaleta@fedoraproject.org>
2011-07-01 15:00:59 -0800

    impliment status grab

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <termios.h> /* POSIX terminal control definitions */
 
 #include "agc_commands.h"
+
+int debug=1;
+
 int main(){
 
 	static char transmitter_port[80]="/dev/ttyS0";
@@ -17,8 +21,11 @@ int main(){
 		1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
 	static int basbox_address=20;
 	static int phasing_address=40;
-	int	transmitter_fd;
+	int	status,i,transmitter_fd;
+        unsigned char command;
+	char	results[25];
 	struct termios options;
+	struct agc_results agc_res;
 
 	printf("Open Serial Ports\n");
     /* open the transmitter port */
@@ -36,10 +43,22 @@ int main(){
     /* set the options */
     tcsetattr(transmitter_fd, TCSANOW, &options);
 
-    
-
 	printf("Transmitter FD: %d\n",transmitter_fd);
-	printf("Close ports\n");
-	close(transmitter_fd);
+	if(transmitter_fd <=0 ) { 
+		fprintf(stderr,"Couldn't open the transmitter port\n");
+		printf("Close ports\n");
+		close(transmitter_fd);
+		exit(-1);
+	}
+	while(1) {
+          command=READ_STATUS;
+	  for(i=0;i<16;i++) {
+	    status=send_command(transmitter_fd,transmitter_addresses[i],command,results,debug);
+            if(status==STATUS) { 
+              agc_results(results,&agc_res);
+            }
+	  }
+	  sleep(60);
+ 	}
   return 0;
 }
