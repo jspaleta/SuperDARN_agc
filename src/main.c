c7008cc34e1ac444877f5b165015511b3b9b6546
Jef Spaleta <jspaleta@fedoraproject.org>
2011-07-04 09:47:49 -0800

    add relay reset

--- src/main.c
+++ src/main.c
@@ -10,15 +10,15 @@
 #include "agc_commands.h"
 
 int debug=1;
-
+int tx=1;
 int main(){
 
-	static char transmitter_port[80]="/dev/ttyS0";
+	static char transmitter_port[80]="/dev/ttyS2";
 	static char basbox_port[80]="/dev/ttyS1";
 	static char phasing_port[80]="/dev/ttyS1";
 
-	static int transmitter_addresses[18]={
-		1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+	static int transmitter_addresses[16]={
+		2,9,15,6,14,8,11,12,5,4,13,18,1,10,16,17};
 	static int basbox_address=20;
 	static int phasing_address=40;
 	int	status,i,transmitter_fd;
@@ -50,14 +50,31 @@ int main(){
 		close(transmitter_fd);
 		exit(-1);
 	}
+        sleep(1);
+	for(i=0;i<16;i++) {
+          //if(transmitter_addresses[i]==tx) {
+            command=TRIP_RELAY;
+	    printf("Transmitter %d : Trip Relay\n",i);
+	    status=send_command(transmitter_fd,transmitter_addresses[i],command,results,debug);
+          sleep(5);
+            command=RESET_RELAY;
+	    printf("Transmitter %d : Reset Relay\n",i);
+	    status=send_command(transmitter_fd,transmitter_addresses[i],command,results,debug);
+          //}
+          sleep(1);
+	}
 	while(1) {
           command=READ_STATUS;
 	  for(i=0;i<16;i++) {
-	    status=send_command(transmitter_fd,transmitter_addresses[i],command,results,debug);
-            if(status==STATUS) { 
-              agc_results(results,&agc_res);
-            }
-	  }
+            if(transmitter_addresses[i]==tx) {
+	      printf("Transmitter %d : Get Status\n",i);
+	      status=send_command(transmitter_fd,transmitter_addresses[i],command,results,debug);
+              if(status==STATUS) { 
+                agc_results(results,&agc_res);
+              }
+	    }
+          }
+          exit(0);
 	  sleep(60);
  	}
   return 0;
