ae5c5a07c3c77d4ad5c1f1325b58588a33efbe7c
root <root@localhost.localdomain>
2013-09-07 21:26:43 -0800

    stuff

---
+++ src/agc_save_ini.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include "iniparser.h"
+#include "agc_commands.h"
+
+void agc_save_results(dictionary *agc_ini,int tx,struct agc_results *res){
+	char entry[120];
+        char filename[120];
+        FILE *fp;
+        sprintf("%s","/tmp/agc.status");
+        fp=fopen(filename,"w+");
+        sprintf(entry,"tx_%d:status_relay",tx);
+        switch(res->relay) {
+	  case CLOSED:
+            iniparser_set(agc_ini,entry,"CLOSED",NULL);
+            break; 
+	  case OPEN:
+            iniparser_set(agc_ini,entry,"OPEN",NULL);
+            break; 
+	  default:
+            printf("Relay: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_loop",tx);
+        switch(res->loop) {
+	  case CLOSED:
+            iniparser_set(agc_ini,entry,"CLOSED",NULL);
+            break; 
+	  case OPEN:
+            iniparser_set(agc_ini,entry,"OPEN",NULL);
+            break; 
+	  default:
+            printf("Loop: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_cap1",tx);
+        switch(res->cap1) {
+	  case IN:
+            iniparser_set(agc_ini,entry,"IN",NULL);
+            break; 
+	  case OUT:
+            iniparser_set(agc_ini,entry,"OUT",NULL);
+            break; 
+	  default:
+            printf("Cap1: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_cap2",tx);
+        switch(res->cap2) {
+	  case IN:
+            iniparser_set(agc_ini,entry,"IN",NULL);
+            break; 
+	  case OUT:
+            iniparser_set(agc_ini,entry,"OUT",NULL);
+            break; 
+	  default:
+            printf("Cap2: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_inhibit",tx);
+        switch(res->inhibit) {
+	  case OFF:
+            iniparser_set(agc_ini,entry,"OFF",NULL);
+            break; 
+	  case ON:
+            iniparser_set(agc_ini,entry,"ON",NULL);
+            break; 
+	  default:
+            printf("Inhibit: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_power",tx);
+        switch(res->power) {
+	  case GOOD:
+            iniparser_set(agc_ini,entry,"GOOD",NULL);
+            break; 
+	  case BAD:
+            iniparser_set(agc_ini,entry,"BAD",NULL);
+            break; 
+	  default:
+            printf("Power: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_dc",tx);
+        switch(res->dc) {
+	  case GOOD:
+            iniparser_set(agc_ini,entry,"GOOD",NULL);
+            break; 
+	  case BAD:
+            iniparser_set(agc_ini,entry,"BAD",NULL);
+            break; 
+	  default:
+            printf("DC: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_vswr",tx);
+        switch(res->vswr) {
+	  case GOOD:
+            iniparser_set(agc_ini,entry,"GOOD",NULL);
+            break; 
+	  case BAD:
+            iniparser_set(agc_ini,entry,"BAD",NULL);
+            break; 
+	  default:
+            printf("VSWR: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_50v",tx);
+        switch(res->dig_50v) {
+	  case GOOD:
+            iniparser_set(agc_ini,entry,"GOOD",NULL);
+            break; 
+	  case BAD:
+            iniparser_set(agc_ini,entry,"BAD",NULL);
+            break; 
+	  default:
+            printf("50v: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_neg15v",tx);
+        switch(res->dig_neg15v) {
+	  case GOOD:
+            iniparser_set(agc_ini,entry,"GOOD",NULL);
+            break; 
+	  case BAD:
+            iniparser_set(agc_ini,entry,"BAD",NULL);
+            break; 
+	  default:
+            printf("neg 15v: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_15v",tx);
+        switch(res->dig_15v) {
+	  case GOOD:
+            iniparser_set(agc_ini,entry,"GOOD",NULL);
+            break; 
+	  case BAD:
+            iniparser_set(agc_ini,entry,"BAD",NULL);
+            break; 
+	  default:
+            printf("neg 15v: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_500v",tx);
+        switch(res->dig_500v) {
+	  case GOOD:
+            iniparser_set(agc_ini,entry,"GOOD",NULL);
+            break; 
+	  case BAD:
+            iniparser_set(agc_ini,entry,"BAD",NULL);
+            break; 
+	  default:
+            printf("500v: Unknown condition\n");
+            break; 
+        }
+        sprintf(entry,"tx_%d:status_5v",tx);
+        switch(res->dig_5v) {
+	  case GOOD:
+            iniparser_set(agc_ini,entry,"GOOD",NULL);
+            break; 
+	  case BAD:
+            iniparser_set(agc_ini,entry,"BAD",NULL);
+            break; 
+	  default:
+            printf("500v: Unknown condition\n");
+            break; 
+        }
+        printf("5v: %d\n",res->alg_5v);
+        printf("50v: %d\n",res->alg_50v);
+        printf("500v: %d\n",res->alg_500v);
+        printf("neg 15v: %d\n",res->alg_15v);
+        printf("pos 15v: %d\n",res->alg_neg15v);
+        printf("temp: %d\n",res->alg_temp);
+        printf("fwd: %d\n",res->alg_fwd);
+        printf("ref: %d\n",res->alg_ref);
+        iniparser_dump_ini(agc_ini,fp);
+        fclose(fp);
+}
--- src/main.c
+++ src/main.c
@@ -11,31 +11,57 @@
 #include "iniparser.h"
 #include "agc_commands.h"
 
-int debug=1;
+int debug=0;
 int verbose=0;
-int tx=-1;
+int tx=0;
+int phasing=0;
+int bas=0;
+int save=0;
+FILE *fp=NULL;
 dictionary *agc_ini;
+struct timeval t_now,t_then;
+int timeout=3600;
+void usage() {
+	printf ("Usage:\n" );
+	printf ("Transmitter Ucontroller manager\n" );
+	printf ("-h / --help : show this message\n");
+	printf ("-c PATH / --configfile PATH : \n configfile");
+	printf ("-o PATH / --output PATH : \n outputfile");
+	printf ("-v / --verbose : increase verbosity by 1\n");
+	printf ("-r / --reset : reset relays\n");
+	printf ("-t / --trip : trip relays\n");
+	printf ("-s / --status : get status\n");
+	printf ("-p / --phasing : talk to phasing matrix\n");
+	printf ("-b / --bas : talk to basbox\n");
+	printf ("-S / --save : save status\n");
+	printf ("-a / --auto : automatically run every hour\n");
+	printf ("-x NUM / --transmitter NUM : \n transmitter postion\n");
+	printf ("If no transmitter selected, loops over all 16 transmitters\n");
+}
 int main(int argc, char **argv){
 
-	static char transmitter_port[80]="/dev/ttyS2";
-	static char basbox_port[80]="/dev/ttyS1";
-	static char phasing_port[80]="/dev/ttyS1";
+	static char transmitter_port[80]="/dev/ttyMP0";
+	//static char basbox_port[80]="/dev/ttyS1";
+	//static char phasing_port[80]="/dev/ttyS1";
 
 	static int tx_positions[19]={
 		0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
 	};
 	static int tx_addresses[19]={
-		0,1,10,16,17,5,4,13,18,14,8,11,12,2,9,15,6,3,7
+		0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,40,0
 	};
-	static int basbox_address=20;
-	static int phasing_address=40;
-	int	status,i,transmitter_fd;
-        unsigned char command;
+	static int tx_disabled[19];
+	static int tx_lockout[19];
+	int	auto_loop=0,flag=1,status,i,transmitter_fd;
+        unsigned char command=0;
 	char	results[25];
 	char	entry[120];
-	char	configfile[120];
+	char	configfile[120]="/data/ros/agc.ini";
+	char	outputfile[120];
 	struct termios options;
 	struct agc_results agc_res;
+	struct pm_results pm_res;
+	struct basbox_results bas_res;
         int arg;                //command line option parsing
 	static struct option long_options[] =
         {
@@ -45,8 +71,13 @@ int main(int argc, char **argv){
 		{"reset_relays", no_argument,      0,      'r'},
 		{"trip_relays",  no_argument,      0,      't'},
 		{"status",  no_argument,      0,      's'},
+		{"phasing",  no_argument,      0,      'p'},
+		{"bas",  no_argument,      0,      'b'},
+		{"save",  no_argument,      0,      'S'},
+		{"atuo",  no_argument,      0,      'a'},
 		{"transmitter",  required_argument,      0,      'x'},
 		{"configfile",  required_argument,      0,      'c'},
+		{"outputfile",  required_argument,      0,      'o'},
 		{"help",         required_argument,      0,      'h'},
 		{0, 0, 0, 0}
         };
@@ -56,7 +87,7 @@ int main(int argc, char **argv){
            /* getopt_long stores the option index here. */
            int option_index = 0;
      
-           arg = getopt_long (argc, argv, "vrstx:c:h",
+           arg = getopt_long (argc, argv, "abpvrsStx:c:o:h",
                             long_options, &option_index);
      
            /* Detect the end of the options. */
@@ -65,6 +96,9 @@ int main(int argc, char **argv){
 
 	switch (arg)
 	{
+		case 'a':
+                  auto_loop=1;
+		break;
 		case 't':
                   command=TRIP_RELAY;
 		break;
@@ -82,20 +116,28 @@ int main(int argc, char **argv){
 		sprintf(configfile,"%s",optarg); 
                 printf("configfile: %s\n",configfile);
 		break;
+		case 'o':
+		sprintf(outputfile,"%s",optarg); 
+                printf("outputfile: %s\n",outputfile);
+		break;
 		case 'v':
 		verbose++;      
+		debug=1;
+		break;
+		case 'p':
+		phasing=1;
+                tx=17;
+		break;
+		case 'b':
+		bas=1;
+                tx=18;
+		break;
+		case 'S':
+		save=1;      
 		break;
 		case 'h':  
 		case '?':
-		printf ("Usage:\n" );
-		printf ("Transmitter Ucontroller manager\n" );
-		printf ("-h / --help : show this message\n");
-		printf ("-c PATH / --configfile PATH : \n configfile");
-		printf ("-v / --verbose : increase verbosity by 1\n");
-		printf ("-r / --reset : reset relays\n");
-		printf ("-t / --trip : trip relays\n");
-		printf ("-x NUM / --transmitter NUM : \n transmitter postion\n");
-		printf ("If no transmitter selected, loops over all 16 transmitters\n");
+                usage();
 		exit(0);  
 		break;
      
@@ -103,82 +145,178 @@ int main(int argc, char **argv){
 		abort();
 	}
         }
-        agc_ini=iniparser_load("agc.ini");
-        iniparser_dump_ini(agc_ini,stdout);
+        if(command==0) {
+          printf("Invalid command, read --help option\n");
+          exit(0);
+        }
+	printf("Open Serial Port: %s\n",iniparser_getstring(agc_ini,"serial_port:dev",transmitter_port));
+        if(phasing) {
+          command=20;
+          if(tx!=17) {
+            printf("Invalid phasing address. Use 17\n");
+            exit(0);
+	  }
+        } else if(bas) {
+          command=14;
+          if(tx!=18) {
+            printf("Invalid bas address. Use 18\n");
+            exit(0);
+	  }
+        } else {
+          if((tx==0)|| (tx > 16) ) {
+            printf("Invalid TX position. Use 1-16  or use -1 to loop\n");
+            exit(0);
+          }
+        }
+        gettimeofday(&t_now,NULL);
+        t_then.tv_sec=t_now.tv_sec-timeout-10;
+        agc_ini=iniparser_load(configfile);
+        //iniparser_dump_ini(agc_ini,stdout);
         for(i=1;i<=16;i++){
           sprintf(entry,"tx_%d:address",i);
           tx_addresses[i]=iniparser_getint(agc_ini,entry,0);
+          sprintf(entry,"tx_%d:disabled",i);
+          tx_disabled[i]=iniparser_getboolean(agc_ini,entry,0);
+          sprintf(entry,"tx_%d:lockout",i);
+          tx_lockout[i]=iniparser_getint(agc_ini,entry,0);
         }
-	printf("Open Serial Ports\n");
-    /* open the transmitter port */
-    transmitter_fd = open(transmitter_port, O_RDWR | O_NOCTTY | O_NDELAY);
-    fcntl(transmitter_fd, F_SETFL, 0);
-    /* get the current options */
-    tcgetattr(transmitter_fd, &options);
-    /* set raw input, 5 second timeout */
-    cfsetispeed(&options, B9600);  //9600 baud
-    options.c_cflag     |= (CS7 | CLOCAL | CREAD);  //7 data bits 1 stop
-    options.c_lflag     &= ~(ICANON | ECHO | ECHOE | ISIG); //raw input
-    options.c_oflag     &= ~OPOST; // raw output
-    options.c_cc[VMIN]  = 0;
-    options.c_cc[VTIME] = 50;
 
-    /* set the options */
-    tcsetattr(transmitter_fd, TCSANOW, &options);
-
-	if(transmitter_fd <=0 ) { 
-		fprintf(stderr,"Couldn't open the transmitter port\n");
-		printf("Close ports\n");
-		close(transmitter_fd);
-		exit(-1);
-	}
-	printf("Transmitter FD: %d\n",transmitter_fd);
-        if((tx==0)|| (tx > 16) ) {
-          printf("Valid TX position is 1-16\n");
-          exit(0);
-        }
-        if(tx>0) {
-	    printf("Transmitter:: Position %d : Address %d: Command: %d\n",tx,tx_addresses[tx],command);
-	    status=send_command(transmitter_fd,tx_addresses[tx],command,results,debug);
-	    if(command==READ_STATUS) {
-              if(status==STATUS) { 
-                agc_results(results,&agc_res);
-                agc_display(&agc_res);
-              }
+	while(flag) {
+          if(auto_loop==1) {
+            gettimeofday(&t_now,NULL);
+            if (abs(t_now.tv_sec-t_then.tv_sec) < timeout) {
+              sleep(1);
+              continue;
             } 
-        }
-        if(tx<0) {
-          printf("Looping over all TX 1-16\n");
-	  for(i=1;i<=16;i++) {
-	  tx=i;
-          sleep(1);
+          } else {
+          }        
+          if(tx>0) {
+            transmitter_fd=tx_port_open(agc_ini,transmitter_port);
+	    printf("Transmitter FD: %d\n",transmitter_fd);
 	    printf("Transmitter:: Position %d : Address %d: Command: %d\n",tx,tx_addresses[tx],command);
-	    status=send_command(transmitter_fd,tx_addresses[tx],command,results,debug);
-	    if(command==READ_STATUS) {
-              if(status==STATUS) { 
-                agc_results(results,&agc_res);
-                agc_display(&agc_res);
+            if(((tx_disabled[tx])==0 && (tx_lockout[tx] < 3 )) || (command==READ_STATUS)) { 
+	      status=send_command(transmitter_fd,tx_addresses[tx],command,results,debug);
+              printf("Command Status,%d\n",status);
+              if(status==TIMEOUT) {
+                printf("Command Timeout, retrying one more time after 5 second wait\n");
+                sleep(5);
+	        status=send_command(transmitter_fd,tx_addresses[tx],command,results,debug);
+                if(status==TIMEOUT) {
+                  printf("Retry Timeout\n");
+		}
               }
-            } 
-          sleep(1);
-	  }
-        }  
-/*
-	while(1) {
-          command=READ_STATUS;
-	  for(i=0;i<16;i++) {
-            if(tx_addresses[i]==tx) {
-	      printf("Transmitter %d : Get Status\n",i);
-	      status=send_command(transmitter_fd,tx_addresses[i],command,results,debug);
-              if(status==STATUS) { 
-                agc_results(results,&agc_res);
-                agc_display(&agc_res);
+	      if(command==20) {
+                if(phasing) {
+                  pm_results(results,&pm_res);
+                  if(verbose > 0 ) pm_display(&pm_res);
+                }  
+                if(phasing) {
+                  basbox_results(results,&bas_res);
+                  if(verbose > 0 ) basbox_display(&bas_res);
+                }  
               }
-	    }
+	      if(command==READ_STATUS) {
+                sprintf(entry,"tx_%d:status",i);
+                switch(status){
+		  case GARBLED:
+                    iniparser_set(agc_ini,entry,"GARBLED",NULL);
+                    break;
+		  case TIMEOUT:
+                    iniparser_set(agc_ini,entry,"TIMEOUT",NULL);
+		    break;
+                  case STATUS:  
+                    agc_results(results,&agc_res);
+                    agc_lockout_test(tx,&agc_res,tx_disabled,tx_lockout);
+                    if(verbose > 0 ) agc_display_results(&agc_res);
+                    agc_save_results(agc_ini,tx,&agc_res,tx_disabled,tx_lockout);
+                    break;
+		  default:
+                    iniparser_set(agc_ini,entry,"UNKNOWN",NULL);
+		    break;
+                } 
+              } 
+            } else {
+              printf("  Transmitter command ignored:: Disabled: %d  Lockout: %d\n",tx_disabled[tx],tx_lockout[tx]);
+            }
           }
-          exit(0);
-	  sleep(60);
- 	}
-*/
+          if(tx<0) {
+            printf("Looping over TX positions 1-16\n");
+	    for(i=1;i<=16;i++) {
+              transmitter_fd=tx_port_open(agc_ini,transmitter_port);
+	      printf("Transmitter FD: %d\n",transmitter_fd);
+	      tx=i;
+	      printf("Transmitter:: Position %d : Address %d: Command: %d\n",tx,tx_addresses[tx],command);
+              if(((tx_disabled[tx])==0 && (tx_lockout[tx] < 3 )) || (command==READ_STATUS)) { 
+                sleep(1);
+	        status=send_command(transmitter_fd,tx_addresses[tx],command,results,debug);
+                printf("Command Status,%d\n",status);
+                if(status==TIMEOUT) {
+                  printf("Command Timeout, retrying one more time after 5 second wait\n");
+                  sleep(5);
+	          status=send_command(transmitter_fd,tx_addresses[tx],command,results,debug);
+                  if(status==TIMEOUT) {
+                    printf("Retry Timeout\n");
+		  }
+                }
+	        if(command==READ_STATUS) {
+                  sprintf(entry,"tx_%d:status",i);
+                  switch(status){
+		    case GARBLED:
+                      iniparser_set(agc_ini,entry,"GARBLED",NULL);
+                      break;
+		    case TIMEOUT:
+                      iniparser_set(agc_ini,entry,"TIMEOUT",NULL);
+		      break;
+                    case STATUS:  
+                      iniparser_set(agc_ini,entry,"STATUS",NULL);
+                      agc_results(results,&agc_res);
+                      agc_lockout_test(tx,&agc_res,tx_disabled,tx_lockout);
+                      if (verbose > 0 ) agc_display_results(&agc_res);
+                      agc_save_results(agc_ini,tx,&agc_res,tx_disabled,tx_lockout);
+                      if(agc_res.relay==OPEN) {
+                        if((tx_disabled[tx]==0) && (tx_lockout[tx] < 3 )) { 
+                          printf("TX %d has open relay: Attempting reset: %d %d\n",tx, tx_disabled[tx],tx_lockout[tx]);
+	                  status=send_command(transmitter_fd,tx_addresses[tx],RESET_RELAY,results,debug);
+                        } else {
+                          printf("TX %d has open relay: and is disabled/lockedout: %d %d\n",tx, tx_disabled[tx],tx_lockout[tx]);
+                        }
+                      }
+                      if(agc_res.relay==CLOSED) {
+                        if(tx_disabled[tx]==1) { 
+                          printf("TX %d is disabled with closed relay: Attempting to trip: %d %d\n",tx, tx_disabled[tx],tx_lockout[tx]);
+	                  status=send_command(transmitter_fd,tx_addresses[tx],TRIP_RELAY,results,debug);
+                        } else {
+                        }
+                      }
+                      break;
+		    default:
+                      iniparser_set(agc_ini,entry,"UNKNOWN",NULL);
+		      break;
+                  }
+                } 
+              } else {
+                printf("  Transmitter command ignored:: Disabled: %d  Lockout: %d\n",tx_disabled[tx],tx_lockout[tx]);
+              }
+	      close(transmitter_fd);
+	    }
+          }  
+	  if(save) {
+            fp=NULL;
+            if(save) fp=fopen(outputfile,"w+");
+	    if(fp!=NULL) {
+	      iniparser_dump_ini(agc_ini,fp);
+	      fclose(fp); 
+	    } else {
+	      if (verbose > 0 ) iniparser_dump_ini(agc_ini,stdout);
+   	    }
+          }
+	  if(auto_loop==1) {
+            t_then.tv_sec=t_now.tv_sec;
+            printf("Waiting %d seconds\n",timeout); 
+	  } else {
+            flag=0;
+	  }
+          transmitter_fd=-1; 
+	} // end the while loop
   return 0;
 }
